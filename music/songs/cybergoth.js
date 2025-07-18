83b13eb77da4b9502a229051b0a9bda3d6418424
Shawn Toubeau <shawntoubeau@gmail.com>
2025-07-18 17:45:14 -0400

    cybergoth progress

--- music/songs/cybergoth.js
+++ music/songs/cybergoth.js
@@ -1,37 +1,45 @@
 await samples('github:ShawnToubeau/samples/master');
 
-setCps(140 / 60 / 4);
+setCps(150 / 60 / 4);
+
+const breakNumCycles = 4
 
 bd$: sound("kick:0")
   .struct("{1!4}%4")  
-  .dist(.8)
-  .lpf("{6000}%4")
-  .attack(0.1)
-  .sustain(.9)
-  .decay(.2)
+  .dist(1.5)
+  .lpf(2000)
   .velocity(perlin.range(.9, 1.3))
   .clip(1) 
 ._scope()
   
-$: sound("fill:0")
+_$: sound("fill:0")
   .struct("{1!4}%4")
   .clip(1)
 
 
-$: sound("cp")
-  .struct("{1 ~}%4")
+_$: sound("cp")
+  .beat("4, 8", 8)
   .clip(1)
 
 $: sound("ch")
-  .coarse(3)
-  .struct("{1!4}%8").sometimesBy(.1, ply("2"))
+  // .dist(1.1)
+  .struct("{1 ~}%8")
+  .sometimesBy(.1, ply("2"))
   .clip(1)
 
 _$: sound("fx")
-  .coarse(10) // 2 | 6
-  .crush(8)
-  .begin(0.15) // .1 | .125 | .15
-  .struct("{1!4}%4")
-  .clip(.75)
-  .gain(.15)
+  .coarse(3)
+  .scrub("{[<0.15@4:1.2> <0@4:.5>]}%16")
+  .hpf(400)
+  .lpf(1200)
+  .clip(1)
+  .gain(.5)
   ._scope()
+
+mel_1: note("{f <a d>}%8".sub(12))
+.s("supersaw")
+.dist(1.5)
+.euclid(15,16)
+.hpf(slider(300,50,1800,50))
+// .postgain(2)
+.clip(1)
